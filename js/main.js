6522c5e1f29c4ee27e250b0317505a5f81a4953c
heraudk <72447916+heraudk@users.noreply.github.com>
2020-11-24 09:36:04 +0100

    jquery exo3

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(function () {
+   
+    $('.div1').on('click', function () {
+        $('.div1').css({ backgroundColor: 'yellow' });
+    });
+})
+
+$(function () {
+    
+    $('.div2').on('click', function () {
+        $('.div2').css({ backgroundColor: 'orange' });
+    });
+})
+
+$(function () {
+    
+    $('.div3').on('click', function () {
+        $('.div3').css({ backgroundColor: 'red' });
+    });
+})
+
+$(function () {
+   
+    $('.div4').on('click', function () {
+        $('.div4').css({ backgroundColor: 'purple' });
+    });
+})
+
